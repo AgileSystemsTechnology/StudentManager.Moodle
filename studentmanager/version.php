d0f1573de0aada3a6b3f506f5f8eba2189a7e4b1
Michael <mike.esteves@gmail.com>
2016-10-10 14:30:33 +0200

    Initialstuff to get basic implementation working

---
+++ studentmanager/version.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Version information
+ *
+ * @package auth_studentmanager
+ * @author Michael Esteves
+ * @license The MIT License (MIT)
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016073100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2016051900;        // Requires this Moodle version
+$plugin->component = 'auth_studentmanager';       // Full name of the plugin (used for diagnostics)
