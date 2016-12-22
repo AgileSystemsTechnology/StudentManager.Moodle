ae73d70f37abdbb39b568d5fc1640b291407cd4e
Michael <mike.esteves@gmail.com>
2016-12-22 08:15:41 +0200

    Fixed for using live url and subdomain

--- studentmanager/version.php
+++ studentmanager/version.php
@@ -10,6 +10,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016073100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016122200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'auth_studentmanager';       // Full name of the plugin (used for diagnostics)
